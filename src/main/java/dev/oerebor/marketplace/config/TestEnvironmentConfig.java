e9699166380223c5132ef84c3598b0399fdee342
Lucas Souza <lucasp.sdev@gmail.com>
2022-08-14 13:35:28 -0300

    feat/order-status (#6)

--- src/main/java/dev/oerebor/marketplace/config/TestEnvironmentConfig.java
+++ src/main/java/dev/oerebor/marketplace/config/TestEnvironmentConfig.java
@@ -2,6 +2,7 @@ package dev.oerebor.marketplace.config;
 
 import dev.oerebor.marketplace.entities.OrderEntity;
 import dev.oerebor.marketplace.entities.UserEntity;
+import dev.oerebor.marketplace.entities.enums.OrderStatus;
 import dev.oerebor.marketplace.repositories.IOrderRepository;
 import dev.oerebor.marketplace.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestEnvironmentConfig implements CommandLineRunner {
         UserEntity u1 = new UserEntity(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         UserEntity u2 = new UserEntity(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
     
-        OrderEntity o1 = new OrderEntity(null, Instant.parse("2022-06-20T19:53:07Z"), u1);
-        OrderEntity o2 = new OrderEntity(null, Instant.parse("2022-07-21T03:42:10Z"), u2);
-        OrderEntity o3 = new OrderEntity(null, Instant.parse("2022-08-07T07:07:07Z"), u1);
+        OrderEntity o1 = new OrderEntity(null, Instant.parse("2022-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        OrderEntity o2 = new OrderEntity(null, Instant.parse("2022-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        OrderEntity o3 = new OrderEntity(null, Instant.parse("2022-08-07T07:07:07Z"), OrderStatus.WAITING_PAYMENT, u1);
         
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));

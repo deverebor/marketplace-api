6d34f4337c56a1b1fa5244bd9d7e3cf691f89076
Lucas Souza <lucasp.sdev@gmail.com>
2022-08-14 10:35:49 -0300

    feat(User): create service and findById endpoint

--- src/main/java/dev/oerebor/marketplace/resources/UserResource.java
+++ src/main/java/dev/oerebor/marketplace/resources/UserResource.java
@@ -1,18 +1,33 @@
 package dev.oerebor.marketplace.resources;
 
 import dev.oerebor.marketplace.entities.UserEntity;
+import dev.oerebor.marketplace.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
     
+    @Autowired
+    private UserService userService;
+    
     @GetMapping
-    public ResponseEntity<UserEntity> findAll() {
-        UserEntity user = new UserEntity(1L, "John Doe", "jhondoe@email.com", "1234567890", "123");
+    public ResponseEntity<List<UserEntity>> findAll() {
+        List<UserEntity> userList = userService.findAll();
+        
+        return ResponseEntity.ok().body(userList);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> findById(@PathVariable Long id) {
+        UserEntity user = userService.findById(id);
         
         return ResponseEntity.ok().body(user);
     }

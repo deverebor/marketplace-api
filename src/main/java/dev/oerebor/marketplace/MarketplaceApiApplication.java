d6484f9af56cc7ee69f92a3bbce6b8380b2365b6
Lucas Souza <lucasp.sdev@gmail.com>
2022-08-14 08:41:18 -0300

    chore: start up app

---
+++ src/main/java/dev/oerebor/marketplace/MarketplaceApiApplication.java
@@ -0,0 +1,13 @@
+package dev.oerebor.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceApiApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/dev/oerebor/marketplace/MarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oerebor.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
